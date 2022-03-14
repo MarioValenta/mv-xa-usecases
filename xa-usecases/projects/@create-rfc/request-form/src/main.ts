275cf76fee9d820ecc4cb8585b9c563a5e946a9f
Matthias Grill <Matthias.Grill@t-systems.com>
2022-03-14 11:22:18 +0100

    @create-rfc: updated ag-grid license

--- xa-usecases/projects/@create-rfc/request-form/src/main.ts
+++ xa-usecases/projects/@create-rfc/request-form/src/main.ts
@@ -4,8 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { CreateRfcRequestFormAppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import {LicenseManager} from 'ag-grid-enterprise';
-LicenseManager.setLicenseKey('CompanyName=T-Systems Austria GesmbH,LicensedGroup=ServiceAutomationPlattform,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=7,LicensedProductionInstancesCount=0,AssetReference=AG-013088,ExpiryDate=21_January_2022_[v2]_MTY0MjcyMzIwMDAwMA==6485b3b3b50740c96e6c4fa0aa65b73d');
+import { LicenseManager } from 'ag-grid-enterprise';
+import { AG_GRID_LICENSE } from '@xa/grid';
+LicenseManager.setLicenseKey(AG_GRID_LICENSE);
 
 if (environment.production) {
   enableProdMode();
