66f43b1e9f6a7159b292707359e772336e62da8e
Matthias Grill <Matthias.Grill@t-systems.com>
2022-04-13 15:51:53 +0200

    @create-snapshot/request-form: updated budget and added ag-grid license

--- xa-usecases/projects/@create-snapshot/request-form/src/main.ts
+++ xa-usecases/projects/@create-snapshot/request-form/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { CreateSnapshotRequestFormAppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { LicenseManager } from 'ag-grid-enterprise';
+import { AG_GRID_LICENSE } from '@xa/grid';
+LicenseManager.setLicenseKey(AG_GRID_LICENSE);
+
 if (environment.production) {
   enableProdMode();
 }
