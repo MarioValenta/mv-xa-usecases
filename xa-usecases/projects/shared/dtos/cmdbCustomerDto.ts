c089717ba68fc0786b22ee39e705d3a556363180
Matthias Grill <Matthias.Grill@t-systems.com>
2022-04-20 16:44:49 +0200

    added common CmdbCustomer DTO

---
+++ xa-usecases/projects/shared/dtos/cmdbCustomerDto.ts
@@ -0,0 +1,11 @@
+export interface CmdbCustomerDto {
+  CUSTOMERABBREVIATION: string;
+  CUSTOMERFULLNAME: string;
+  CUSTOMERICUCLIENT: string;
+  CUSTOMERICUCORPORATION: string;
+  CUSTOMERID: string;
+  CUSTOMERNICKNAME: string;
+  CUSTOMERSAPABBR: string;
+  sm9name: string;
+  label: string;
+}
